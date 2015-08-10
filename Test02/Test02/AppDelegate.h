16810bd5a5abb6d24c0e4013225d1654e96c0c04
Tony <Tony@TonydeMac-mini.local>
2015-08-10 21:38:11 +0800

    First commit

---
+++ Test02/Test02/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test02
+//
+//  Created by Tony on 15/8/10.
+//  Copyright (c) 2015年 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
